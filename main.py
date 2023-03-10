13c728cf8c1182dff42296b5b2f05e64c3fae82d
Frank <65999885+FrankXie05@users.noreply.github.com>
2023-03-10 16:37:05 +0800

    Update main.py
    函数封装

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 import pandas as pd
@@ -8,6 +7,9 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from bs4 import BeautifulSoup
 import re
+from github_port import github_port
+from get_repo import get_repo
+from get_vcpkg_version import get_vcpkg_version
 
 # 邮件发送者和接收者
 sender = 'sender@qq.com'
@@ -34,37 +36,18 @@ if response.status_code == 200:
     for link in links:
             port_name.append(link['title'])
 
-    # 读取每个端口的vcpkg.json文件并提取名称和版本信息
-    data_list = []
-    cont = 0
-    port_names = str(port_name)
-    for i, port_link in enumerate(port_name):
-        json_url = 'https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/' + port_link + '/vcpkg.json'
-        json_response = requests.get(json_url)
-        if json_response.status_code == 200:
-            content = json_response.content.decode('utf-8')
-            data = json.loads(content)
-            if 'version' in data:
-                Version = data.get('version')
-            elif 'version-date' in data:
-                Version = data.get('version-date')
-            elif 'version-semver' in data:
-                Version = data.get('version-semver')
-            elif 'version-string' in data:
-                Version = data.get('version-string')
-            else:
-                Version = None
+    # 调用get_vcpkg_version
+    content,vcpkg_data_list = get_vcpkg_version(port_name)
 
-            name = data.get('name')
-            data_list.append({'Name': name, 'Version': Version})
-            cont ++ 1
-            print(f"{i+1}/{len(port_name)}: {port_link} Done")
-        else:
-            print(f"Failed to retrieve {json_url}.")
+    #调用github_repos
+    github_repos = github_port()
+
+    #调用get_repos
+    github_repo = get_repo(content)
     
     # 将数据转换为表格
-    df = pd.DataFrame(data_list)
-    print(df)
+    vcpkg_version_df = pd.DataFrame(vcpkg_data_list)
+    print(vcpkg_version_df)
 
     # 创建邮件对象
     msg = MIMEMultipart()
@@ -73,7 +56,7 @@ if response.status_code == 200:
     msg['Subject'] = 'vcpkg.json 表格数据'
 
     # 将表格数据转换为csv格式，并作为附件添加到邮件中
-    csv_data = df.to_csv(index=False)
+    csv_data = vcpkg_version_df.to_csv(index=False)
     attachment = MIMEApplication(csv_data.encode(), Name='vcpkg.csv')
     attachment['Content-Disposition'] = 'attachment; filename="vcpkg.csv"'
     msg.attach(attachment)

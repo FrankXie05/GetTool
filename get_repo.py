2563a5a470cff00d9bbeab9eedaa8d5964c2987b
Frank <65999885+FrankXie05@users.noreply.github.com>
2023-03-13 15:31:41 +0800

    Update get_repo.py

--- get_repo.py
+++ get_repo.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-def get_repo(content: str) -> str:
+def get_repo(port_name,content: str) -> str:
     """
     This function extracts the github repository information from a given string.
 
@@ -16,28 +16,22 @@ def get_repo(content: str) -> str:
     # 判断repo地址
 
     if 'vcpkg_from_github(\n' in content:
-        github_repo = re.search(r"vcpkg_from_github\(\n", content)
-        if github_repo:
-            return github_repo.group(1)
+        get_repo = re.search(r"vcpkg_from_github\(\n", content)
 
     elif 'vcpkg_from_gitlab(\n' in content:
-        gitlab_repo = re.search(r"vcpkg_from_gitlab\(\n", content)
-        if gitlab_repo:
-            return gitlab_repo.group(1)
+        get_repo = re.search(r"vcpkg_from_gitlab\(\n", content)
 
     elif 'vcpkg_from_git(\n' in content:
-        git_repo = re.search(r"vcpkg_from_git\(\n", content)
-        if git_repo:
-            return git_repo.group(1)
+        get_repo = re.search(r"vcpkg_from_git\(\n", content)
 
     elif 'vcpkg_from_sourceforge(\n' in content:
-        sourceforge_repo = re.search(r"vcpkg_from_sourceforge\(\n", content)
-        if sourceforge_repo:
-            return sourceforge_repo.group(1)
+        get_repo = re.search(r"vcpkg_from_sourceforge\(\n", content)
 
     elif 'vcpkg_download_distfile(ARCHIVE\r\n' in content:
-        other_repo = re.search(r"vcpkg_download_distfile\(ARCHIVE\r\n", content)
-        if other_repo:
-            return other_repo.group(1)
+        get_repo = re.search(r"vcpkg_download_distfile\(ARCHIVE\r\n", content)
+        
+    if get_repo:
+        print(f"{port_name}: {get_repo.group(0)}")
+        return get_repo
 
     return None

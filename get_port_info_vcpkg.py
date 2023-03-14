d9a98f8db363291242b38912903bf2e3de1e7579
Frank <65999885+FrankXie05@users.noreply.github.com>
2023-03-14 18:48:05 +0800

    Update get_vcpkg_version.py

--- get_port_info_vcpkg.py
+++ get_port_info_vcpkg.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-def get_vcpkg_version(port_name:str) -> list:
+def get_port_info_vcpkg(url,port_names:str) -> list:
 	
-    # 读取每个端口的vcpkg.json文件并提取名称和版本信息
+    
     vcpkg_data_list = []
-    port_names = str(port_name)
-    for i, port_link in enumerate(port_name):
-        json_url = 'https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/' + port_link + '/vcpkg.json'
+    for i, port_name in enumerate(port_names):
+        # 读取每个端口的vcpkg.json文件并提取名称和版本信息
+        json_url = 'https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/' + port_name + '/vcpkg.json'
         json_response = requests.get(json_url)
         if json_response.status_code == 200:
             content = json_response.content.decode('utf-8')
@@ -25,7 +25,7 @@ def get_vcpkg_version(port_name:str) -> list:
 
             name = vcpkg_data.get('name')
             vcpkg_data_list.append({'Name': name, 'Version': Version})
-            print(f"{i+1}/{len(port_name)}: {port_link} Done")
+            print(f"{i+1}/{len(port_names)}: {port_name} Done")
         else:
             print(f"Failed to retrieve {json_url}.")
 
